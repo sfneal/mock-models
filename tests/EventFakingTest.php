a17c415d0220a72342a1eaec64c2b6e43e32b3cd
Stephen Neal <stephen.neal14@gmail.com>
2021-04-21 11:42:01 -0400

    MAKE tests for `CreateRequest`, `EventFaker` & `QueueFaker` traits

---
+++ tests/EventFakingTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Sfneal\Testing\Tests;
+
+
+use Illuminate\Support\Facades\Event;
+use Sfneal\Testing\Utils\Traits\EventFaker;
+
+class EventFakingTest extends TestCase
+{
+    use EventFaker;
+
+    /** @test */
+    public function events_can_be_faked()
+    {
+        $this->eventFaker();
+
+        Event::assertNothingDispatched();
+    }
+}
---
+++ tests/QueueFakingTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Sfneal\Testing\Tests;
+
+
+use Illuminate\Support\Facades\Queue;
+use Sfneal\Testing\Utils\Traits\QueueFaker;
+
+class QueueFakingTest extends TestCase
+{
+    use QueueFaker;
+
+    /** @test */
+    public function events_can_be_faked()
+    {
+        $this->queueFaker();
+
+        Queue::assertNothingPushed();
+    }
+}
